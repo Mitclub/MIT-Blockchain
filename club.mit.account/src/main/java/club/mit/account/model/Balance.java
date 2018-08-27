859a76d9a929a19d4f1969a20912adbd623c0689
mit <mit.club@yandex.com>
2018-08-27 17:28:14 +0800

    update Balance.java/club/mit/account/model/Balance.java

--- club.mit.account/src/main/java/club/mit/account/model/Balance.java
+++ club.mit.account/src/main/java/club/mit/account/model/Balance.java
@@ -3,19 +3,24 @@ package club.mit.account.model;
 public class Balance {
 	private long balance;
 	private boolean locked;
+
 	@Override
 	public String toString() {
 		return "Balance [balance=" + balance + ", locked=" + locked + "]";
 	}
+
 	public long getBalance() {
 		return balance;
 	}
+
 	public void setBalance(long balance) {
 		this.balance = balance;
 	}
+
 	public boolean isLocked() {
 		return locked;
 	}
+
 	public void setLocked(boolean locked) {
 		this.locked = locked;
 	}
