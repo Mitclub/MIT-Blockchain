2f7338d7963607e1aba083757ef385c6d6b7991f
mit <mit.club@yandex.com>
2018-08-30 10:29:05 +0800

    add Consts

--- club.mit.account/src/main/java/club/mit/account/model/Consts.java
+++ club.mit.account/src/main/java/club/mit/account/model/Consts.java
@@ -1,5 +1,9 @@
 package club.mit.account.model;
 
 public class Consts {
-
+	// 账户最大解锁时长
+	public static final int AccountMaxUnlockTime = 600;
+	
+	// 账号文件扩展名
+	public static final String KeyStoreFileExtension = ".keystore";
 }
